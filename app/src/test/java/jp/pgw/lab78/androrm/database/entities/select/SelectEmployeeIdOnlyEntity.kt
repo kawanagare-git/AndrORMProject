4ae6896e838e3f819851ad5946ec9330a91f9616
MasahiroKawanagare <41260526+MasahiroKawanagare@users.noreply.github.com>
2025-07-17 21:45:53 +0900

    2025/07/13 変更分

--- app/src/test/java/jp/pgw/lab78/androrm/database/entities/select/SelectEmployeeIdOnlyEntity.kt
+++
@@ -1,9 +0,0 @@
-package jp.pgw.lab78.androrm.database.entities.select
-
-import jp.pgw.lab78.androrm.annotation.Table
-import jp.pgw.lab78.androrm.database.interfaces.entity.SelectEntity
-
-@Table("EMPLOYEE",alias = "EMO")
-data class SelectEmployeeIdOnlyEntity(
-    val employeeId: String,
-) : SelectEntity
