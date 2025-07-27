b6f1d1c9101442c9a01e5fdb179fb419b53735f1
MasahiroKawanagare <41260526+MasahiroKawanagare@users.noreply.github.com>
2025-07-27 15:53:20 +0900

    2025/07/27 data class パッケージ付与・インターフェス実装を完了

---
+++ androrm-common/src/main/java/jp/pgw/lab78/androrm/common/dml/DMLInterfaceEnum.kt
@@ -0,0 +1,23 @@
+package jp.pgw.lab78.androrm.common.dml
+
+import jp.pgw.lab78.androrm.common.Constants.EMPTY_STRING
+import jp.pgw.lab78.androrm.common.dml.interfaces.InsertEntity
+import jp.pgw.lab78.androrm.common.dml.interfaces.SelectEntity
+import jp.pgw.lab78.androrm.common.dml.interfaces.UpdateEntity
+import jp.pgw.lab78.androrm.common.dml.interfaces.UpsertEntity
+
+/**
+ * ## 共通インターフェス定義列挙型
+ * ### AndrORM のインターフェース列挙型
+ * ### @Projection.DMLInterfaceEnum に指定する
+ * @param interfaceFQN 共通インターフェスの FQN
+ */
+public enum class DMLInterfaceEnum(
+    /** 共通インターフェスの FQN */
+    public  val interfaceFQN: String) {
+    NOT_USE(EMPTY_STRING),
+    SELECT(SelectEntity::class.qualifiedName.toString()),
+    INSERT(InsertEntity::class.qualifiedName.toString()),
+    UPDATE(UpdateEntity::class.qualifiedName.toString()),
+    UPSERT(UpsertEntity::class.qualifiedName.toString()),
+}
