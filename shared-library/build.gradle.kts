7adce6186f537ec085978cc9b2fff8895a3f2b9a
Masahiro Kawanagare Inoue <maspost.rx78.ga00821@gmail.com>
2025-10-19 20:54:41 +0900

    2025/10/19 Having メソッド改良後

---
+++ shared-library/build.gradle.kts
@@ -0,0 +1,30 @@
+// ＜shared-library/build.gradle.kts＞
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
+
+plugins {
+    id("java-library")
+    kotlin("jvm")
+}
+
+// Kotlin JVM 設定（Toolchain指定）
+extensions.configure<KotlinJvmProjectExtension>("kotlin") {
+    jvmToolchain(17)
+}
+
+dependencies {
+    // Kotlin 標準ライブラリ
+    implementation(kotlin("stdlib"))
+    implementation(libs.kotlin.reflect)
+
+    // 共通ユーティリティ（必要ならログなど）
+    implementation(libs.slf4j.api)
+    implementation(libs.logback.android)
+
+    // テストライブラリ
+    testImplementation(libs.junit.jupiter)
+    testImplementation(libs.mockito.core)
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+}
